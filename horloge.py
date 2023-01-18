772852bd66fade95ea866532a80b9c6dde1d6d6a
warda-mohtaj <115171252+warda-mohtaj@users.noreply.github.com>
2023-01-18 08:21:58 +0100

    Add files via upload

---
+++ horloge.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+
+updated = ( datetime.now() +
+		timedelta( hours=0 )).strftime('%H:%M:%S')
+
+print( updated )
+
+#j'ai essayée pendant 2 jours le pour aller plus loin j'ai demandée de l'aide aussi j'ai pas reussis je prefére continuer et laisser.
---
+++ horloge2.py
@@ -0,0 +1,16 @@
+horloge = int(input ( " Entrez le nombre de secondes voulue : "))
+# question pour les nombres de secondes 
+
+secondes = horloge % (24 * 3600)
+#nombres de secondes par jour 
+heures = horloge // 3600
+#nombre d'heures en secondes
+secondes %= 3600
+minutes = horloge // 60
+#nombre de seconde en 1 min
+secondes %= 60 
+
+print("%d:%02d:%02d" % (heures, minutes, secondes))
+#affiche l'heure final
+
+#mon deuxième code qui marche avec le nombre de secondes donner
